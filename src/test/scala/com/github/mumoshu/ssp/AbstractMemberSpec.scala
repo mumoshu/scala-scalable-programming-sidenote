25fb854dc3200fef638ed576519d3412e3a25b63
mumoshu <ykuoka@gmail.com>
2012-04-23 23:30:28 +0900

    spec

---
+++ src/test/scala/com/github/mumoshu/ssp/AbstractMemberSpec.scala
@@ -0,0 +1,13 @@
+package com.github.mumoshu.ssp
+
+import org.specs2.mutable.Specification
+
+object AbstractMemberSpec extends Specification {
+
+  "Foo" should {
+    "Bar" in {
+      true should beEqualTo (true)
+    }
+  }
+
+}
